7bb5d6918e6e94461c7cc5a85fdc39fdfbb0778b
blubber4456 <blubber4456@gmail.com>
2018-05-09 13:57:42 +0100

    Add files via upload

---
+++ Powershell/SID.ps1
@@ -0,0 +1,5 @@
+﻿$objUser = New-Object System.Security.Principal.NTAccount("Markit.Partners", "Dave.Allen")
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier]) 
+$strSID.Value
+##Simple:
+##Get-ADUser -Server info.corp -Identity 'honeypot' | select SID
---
+++ Powershell/User Migrated check.ps1
@@ -0,0 +1,9 @@
+﻿$Server = Read-Host -Prompt 'Input your server name'
+$User = Read-Host -Prompt 'Input the user name'
+#Write-Host "You input server '$Server' and '$User' on '$Date'" 
+#get-aduser -server $server -Identity $user -Properties extensionAttribute13 | select GivenName,Surname,extensionAttribute13
+$ADAtt = Get-aduser -server $server -Identity $user -Properties extensionAttribute13 | select extensionAttribute13
+#Write-host "testing '$ADUser' Testing"
+#if($ADUser=Complete){Write-host "BOO YAH"}
+Write-host "The status is: '$ADAtt' If it says Completed the mail is defnitly migrated"
+Read-Host -Prompt "Press Enter to continue"
